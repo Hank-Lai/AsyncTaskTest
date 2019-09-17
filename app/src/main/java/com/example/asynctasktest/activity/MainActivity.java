fc4e23f725124b0d5952fb0415162d6029f8c976
Hank.Lai <a22611549@gmail.com>
2019-09-17 09:15:21 +0800

    First version

---
+++ app/src/main/java/com/example/asynctasktest/activity/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.asynctasktest.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.asynctasktest.R;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button testbtn = null;
+    private Button changebtn = null;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progress_bar);
+        this.testbtn = (Button) this.findViewById(R.id.button);
+        this.changebtn = this.findViewById(R.id.button1);
+        this.testbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAsyncTask(view);
+            }
+        });
+        this.changebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, RecyclerActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void startAsyncTask(View v) {
+        ExampleAsyncTask task = new ExampleAsyncTask(this);
+        task.execute(10);
+    }
+
+    private static class ExampleAsyncTask extends AsyncTask<Integer, Integer, String> {
+        private WeakReference<MainActivity> activityWeakReference;
+
+        ExampleAsyncTask(MainActivity activity) {
+            activityWeakReference = new WeakReference<MainActivity>(activity);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            activity.progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            for (int i = 0; i < integers[0]; i++) {
+                publishProgress((i * 100) / integers[0]);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Finished!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            activity.progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+
+            Toast.makeText(activity, s, Toast.LENGTH_SHORT).show();
+            activity.progressBar.setProgress(0);
+            activity.progressBar.setVisibility(View.INVISIBLE);
+        }
+
+    }
+}
